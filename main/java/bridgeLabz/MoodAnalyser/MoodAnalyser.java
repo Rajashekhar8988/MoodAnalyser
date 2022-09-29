73ec20849249cfe7d4da4216a3d62a65d60597cc
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-29 22:17:36 +0530

    modified : repeat TC1.2 i am in happy mood constructor message should return SAD

--- main/java/bridgeLabz/MoodAnalyser/MoodAnalyser.java
+++ main/java/bridgeLabz/MoodAnalyser/MoodAnalyser.java
@@ -3,14 +3,16 @@ package bridgeLabz.MoodAnalyser;
 public class MoodAnalyser {
 	
     public String message;
-	//
+	MoodAnalyser(){
+		
+	}
+	//constructor with message to analyze mood
 	MoodAnalyser(String message){
 		this.message=message;
 	}
 	//method to analyze mood by reading string
 	public String analyseMood() {
-	
-		if (message.toLowerCase().contains("sad")) {
+		if (message.contains("Happy")) {
 			return "SAD";
 		} else {
 			return "HAPPY";
@@ -18,7 +20,7 @@ public class MoodAnalyser {
 		}	
 	//main method
 	public static void main(String[] args) {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am sad in mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am Happy in mood");
 		String mood = moodAnalyser.analyseMood();
 		System.out.println(mood);
 	}
--- test/java/bridgeLabz/MoodAnalyser/MoodAnalyserTest.java
+++ test/java/bridgeLabz/MoodAnalyser/MoodAnalyserTest.java
@@ -4,10 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
+	//Test case using I am Happy in mood constructor message to return SAD
 	@Test
 	public void givenMessage_IsProper_ShouldReturnSad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser("I am sad in mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am Happy in mood");
 		String actualResult = moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD", actualResult);
 	}
